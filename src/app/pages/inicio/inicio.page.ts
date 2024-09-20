acc94de9e3835c98e8936e4f2ee89481cc458789
Camiliink <cam.jopia@duocuc.cl>
2024-09-20 00:01:30 -0300

    ANIMACIONES Y CAMBIOS ESTETICOS
    PAGINA INICIO-MISDATOS-QR CON CAMBIOS ESTÉTICOS

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,10 +1,11 @@
+import { CorreoPage } from './../correo/correo.page';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { NivelEducacional } from 'src/app/model/nivel-educacional';
 import { Usuario } from 'src/app/model/usuario';
 import { AnimationController} from '@ionic/angular';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, animation, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-inicio',
@@ -16,15 +17,12 @@ export class InicioPage implements AfterViewInit {
 
   @ViewChild('titulo', { read: ElementRef }) itemTitulo!: ElementRef;
   @ViewChild('page', { read: ElementRef }) page!: ElementRef;
-  @ViewChild('itemCuenta', { read: ElementRef }) itemCuenta!: ElementRef;
-  @ViewChild('itemNombre', { read: ElementRef }) itemNombre!: ElementRef;
-  @ViewChild('itemApellido', { read: ElementRef }) itemApellido!: ElementRef;
-  @ViewChild('itemEducacion', { read: ElementRef }) itemEducacion!: ElementRef;
-  @ViewChild('itemFechaNacimiento', { read: ElementRef }) itemFechaNacimiento!: ElementRef;
+  @ViewChild('mensajeQR', {read: ElementRef})itemMensajeQR! : ElementRef;
   
-  public listaNivelesEducacionales = NivelEducacional.getNivelesEducacionales();
   
   public usuario: Usuario;
+  cardElements: any;
+  
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -39,37 +37,7 @@ export class InicioPage implements AfterViewInit {
   ngAfterViewInit() {
     this.animarTituloIzqDer();
     this.animarVueltaDePagina();
-  }
-
-  public actualizarNivelEducacional(event: any) {
-    this.usuario.nivelEducacional 
-      = NivelEducacional.buscarNivelEducacional(event.detail.value)!;
-  }
-
-  limpiarPagina() {
-    this.usuario.cuenta = '';
-    this.usuario.nombre = '';
-    this.usuario.apellido = '';
-    this.usuario.nivelEducacional = NivelEducacional.buscarNivelEducacional(1)!;
-    this.usuario.fechaNacimiento = undefined;
-  }
-
-  limpiarAnimandoDerIzq() {
-    this.limpiarPagina();
-    this.animarDerIzq(this.itemCuenta.nativeElement, 100);
-    this.animarDerIzq(this.itemNombre.nativeElement, 200);
-    this.animarDerIzq(this.itemApellido.nativeElement, 300);
-    this.animarDerIzq(this.itemEducacion.nativeElement, 400);
-    this.animarDerIzq(this.itemFechaNacimiento.nativeElement, 500);
-  }
-
-  limpiarAnimandoRotacion() {
-    this.limpiarPagina();
-    this.animarRotacion(this.itemCuenta.nativeElement, 800);
-    this.animarRotacion(this.itemNombre.nativeElement, 1100);
-    this.animarRotacion(this.itemApellido.nativeElement, 1400);
-    this.animarRotacion(this.itemEducacion.nativeElement, 1700);
-    this.animarRotacion(this.itemFechaNacimiento.nativeElement, 2000);
+    this.mensajeQR();
   }
 
   animarTituloIzqDer() {
@@ -121,35 +89,6 @@ export class InicioPage implements AfterViewInit {
     return 'No asignado';
   }
 
-  mostrarDatosPersona() {
-    // Si el usuario no ingresa la cuenta, se mostrará un error
-    if (this.usuario.cuenta.trim() === '') {
-      this.mostrarMensajeAlerta('La cuenta es un campo obligatorio.');
-      return;
-    }
-
-    // Si el usuario no ingresa al menos el nombre o el apellido, se mostrará un error
-    this.usuario.nombre = this.usuario.nombre.trim();
-    this.usuario.apellido = this.usuario.apellido.trim();
-    if (this.usuario.nombre.trim() === '' && this.usuario.apellido === '') {
-      this.mostrarMensajeAlerta('Debe ingresar al menos un nombre o un apellido.');
-      return;
-    }
-
-    // Mostrar un mensaje emergente con los datos de la persona
-    let mensaje = `
-      <small>
-        <b>Cuenta:     </b> ${this.usuario.cuenta} <br>
-        <b>Usuario:    </b> ${this.usuario.correo} <br>
-        <b>Nombre:     </b> ${this.asignado(this.usuario.nombre)} <br>
-        <b>Apellido:   </b> ${this.asignado(this.usuario.apellido)} <br>
-        <b>Educación:  </b> ${this.asignado(this.usuario.nivelEducacional.getEducacion())} <br>
-        <b>Nacimiento: </b> ${this.usuario.getFechaNacimiento()}
-      </small>
-    `;
-    this.mostrarMensajeAlerta(mensaje);
-  }
-
   async mostrarMensajeAlerta(mensaje: string) {
     const alert = await this.alertController.create({
       header: 'Datos personales',
@@ -162,5 +101,20 @@ export class InicioPage implements AfterViewInit {
   navegar(pagina: string) {
     this.usuario.navegarEnviandousuario(this.router, pagina);
   }
+
+  mensajeQR(){
+    this.animationController
+    .create()
+    .addElement(this.itemMensajeQR.nativeElement)
+    .iterations(Infinity)
+    .duration(2000)
+    .keyframes([
+      { offset: 0, transform: 'scale(1)', opacity: '1' },
+      { offset: 0.5, transform: 'scale(1.5)', opacity: '0.3' },
+      { offset: 1, transform: 'scale(1)', opacity: '1' },
+    ])
+    .play()
+  }
+
 
 }
